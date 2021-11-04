791cdc4634e540dfb178458748fe3f4103b28492
fiyinfoluwa20 <93296594+fiyinfoluwa20@users.noreply.github.com>
2021-11-04 01:12:11 +0100

    Update define.php

--- app/auth/define.php
+++ app/auth/define.php
@@ -1,10 +1,5 @@
 <?php
-// define("HOST", 'YOUR_DATABASE_HOST');
-// define('PASSWORD', 'YOUR_DATABASE_PASSWORD');
-// define("USER", 'YOUR_DATABASE_USER');
-// define("DB_NAME", 'YOUR_DATABASE_NAME');
-
-define("HOST", 'localhost');
-define('PASSWORD', '');
-define("USER", 'root');
-define("DB_NAME", 'si');
+define("HOST", 'YOUR_DATABASE_HOST');
+define('PASSWORD', 'YOUR_DATABASE_PASSWORD');
+define("USER", 'YOUR_DATABASE_USER');
+define("DB_NAME", 'YOUR_DATABASE_NAME');
